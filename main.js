3abb26498d8534e826834ca129c47481061bc810
simonescoca <simone.scoca@icloud.com>
2023-05-31 17:13:56 +0200

    created posts with a func and a forEach

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // # Descrizione
     // * Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
-    // * Non è necessario creare date casuali Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es.
-    // * Unsplash (https://unsplash.it/300/300?image=<id>)
+        // * Non è necessario creare date casuali Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es.
+        // * Unsplash (https://unsplash.it/300/300?image=<id>)
 
 // # Milestone 1
     // * Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
@@ -75,3 +75,71 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const containerElement = document.getElementById ("container")
+
+posts.forEach ((obj) => {
+    const id = obj.id
+    const content = obj.content
+    const media = obj.media
+    const {name, image} = obj.author
+    const likes = obj.likes
+    const created = obj.created
+    
+    createPost(id, content, media, name, image, likes, created)
+})
+
+
+// ! FUNCTIONS ......................................................................
+/**
+ * Creates a post populating container with innerHTML
+ * @param {number} id 
+ * @param {string} content 
+ * @param {string} media 
+ * @param {string} name 
+ * @param {string} image 
+ * @param {number} likes 
+ * @param {string} created 
+ */
+function createPost (id, content, media, name, image, likes, created) {
+    containerElement.innerHTML += `
+        <div class="post" id="n${id}">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${image}" alt="${name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${name}</div>
+                        <div class="post-meta__time">${created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">
+                ${content}
+            </div>
+            <div class="post__image">
+                <img src="${media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">
+                                Mi Piace
+                            </span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a
+                        <b id="like-counter-1" class="js-likes-counter">
+                            ${likes}
+                        </b>
+                        persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `
+}
